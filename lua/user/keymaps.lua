041d9bec2b65be59b8512900ee4d46258c1bc07a
YKPAIHELIb <11073387+YKPAIHELIb@users.noreply.github.com>
2023-11-21 00:59:59 +0200

    Added git_files keymap, changed live_grep to <leader>sr

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -98,7 +98,8 @@ end
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>sr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
 
