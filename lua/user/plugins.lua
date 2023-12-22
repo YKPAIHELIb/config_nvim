40d5a59bfdd130090e087a6b11b4ca2c0aed69be
YKPAIHELIb <11073387+YKPAIHELIb@users.noreply.github.com>
2023-12-22 12:45:10 +0200

    Added dirbuf plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,6 +96,8 @@ return packer.startup(function(use)
   use 'simrat39/rust-tools.nvim'
   use 'mfussenegger/nvim-dap'
 
+  use "elihunter173/dirbuf.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
